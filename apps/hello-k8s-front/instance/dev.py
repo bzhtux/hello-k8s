61053ad8d0c52d94a898a255a9b0b550a0ab8a44
Yannick Foeillet <bzhtux@gmail.com>
2019-04-11 17:59:08 +0200

    fix(app): Fix and clean old fashion to fetch env vars

--- apps/hello-k8s-front/instance/dev.py
+++ apps/hello-k8s-front/instance/dev.py
@@ -4,17 +4,16 @@ import os
 ### HELLO-K8S-FRONTEND  # noqa
 ######################
 try:
-    os.environ['HKF_DEBUG'] != ""
     HKF_DEBUG = os.environ['HKF_DEBUG']
 finally:
     HKF_DEBUG = False
+
 try:
-    os.environ['HKF_HOST'] != ""
     HKF_HOST = os.environ['HKF_HOST']
 finally:
     HKF_HOST = "0.0.0.0"
+
 try:
-    os.environ['HKF_PORT'] != ""
     HKF_PORT = os.environ['HKF_PORT']
 finally:
     HKF_PORT = 8080
@@ -25,13 +24,11 @@ MAX_MSG_GET = 10
 ### API  # noqa
 #######
 try:
-    os.environ['API_HOST'] != ""
     API_HOST = os.environ['API_HOST']
 finally:
     API_HOST = "0.0.0.0"
 
 try:
-    os.environ['API_PORT'] != ""
     API_PORT = int(os.environ['API_PORT'])
 finally:
     API_PORT = 5000
