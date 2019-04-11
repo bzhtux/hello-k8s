dc518313e766671cd9b5d172e0f8e864168dddf4
Yannick Foeillet <bzhtux@gmail.com>
2019-04-11 17:59:08 +0200

    fix(app): Fix and clean old fashion to fetch env vars

--- apps/hello-k8s-api/instance/dev.py
+++ apps/hello-k8s-api/instance/dev.py
@@ -5,33 +5,31 @@ import os
 ### HELLO-K8S-API  # noqa
 #################
 try:
-    os.environ['HKA_HOST'] != ""
     HKA_HOST = os.environ['HKA_HOST']
 finally:
     HKA_HOST = "0.0.0.0"
+
 try:
-    os.environ['HKA_PORT'] != ""
     HKA_PORT = os.environ['HKA_PORT']
 finally:
     HKA_PORT = 5000
+
 try:
-    os.environ['HKA_DEBUG'] != ""
     HKA_DEBUG = os.environ['HKA_DEBUG']
 finally:
     HKA_DEBUG = True
+
 MAX_MSG_GET = 10
 
 #########
 ### REDIS  # noqa
 #########
 try:
-    os.environ['REDIS_HOST'] != ""
     REDIS_HOST = os.environ['REDIS_HOST']
 finally:
     REDIS_HOST = "0.0.0.0"
 
 try:
-    os.environ['REDIS_PORT'] != ""
     REDIS_PORT = int(os.environ['REDIS_PORT'])
 finally:
     REDIS_PORT = 6379
