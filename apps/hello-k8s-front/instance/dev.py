f808f9416f2601ff9e440aebefea623b8f1185da
Yannick Foeillet <bzhtux@gmail.com>
2019-04-11 17:34:00 +0200

    fix(app): Fix missing env vars to catch

--- apps/hello-k8s-front/instance/dev.py
+++ apps/hello-k8s-front/instance/dev.py
@@ -3,9 +3,18 @@ import os
 ######################
 ### HELLO-K8S-FRONTEND  # noqa
 ######################
-HKF_DEBUG = True
-HKF_HOST = "0.0.0.0"
-HKF_PORT = 8080
+if os.environ['HKF_DEBUG'] != "":
+    HKF_DEBUG = os.environ['HKF_DEBUG']
+else:
+    HKF_DEBUG = False
+if os.environ['HKF_HOST'] != "":
+    HKF_HOST = os.environ['HKF_HOST']
+else:
+    HKF_HOST = "0.0.0.0"
+if os.environ['HKF_PORT'] != "":
+    HKF_PORT = os.environ['HKF_PORT']
+else:
+    HKF_PORT = 8080
 MAX_MSG_GET = 10
 
 
